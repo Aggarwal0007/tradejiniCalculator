910e925b08bb3826d24f0d3d70eaf277ebeccb69
Keerthi O P <keerthi.op@iouring.com>
2022-04-01 10:25:21 +0530

    #692: counter animation done

--- src/components/brokerage/BrokerageOutputsComponent.tsx
+++ src/components/brokerage/BrokerageOutputsComponent.tsx
@@ -6,6 +6,22 @@ const BrokerageOutputs = (props: { chargesBreakDown: Charges; }) => {
     const { chargesBreakDown } = props;
 
     const [
+        turnoverArr, setTurnoverArr
+    ] = useState<any>();
+
+    const [
+        brokerageArr, setBrokerageArr
+    ] = useState<any>();
+
+    const [
+        bepArr, setBepArr
+    ] = useState<any>();
+
+    const [
+        profitArr, setProfitArr
+    ] = useState<any>();
+
+    const [
         brokerageValue, setBrokerageValue
     ] = useState<string | number>(40);
 
@@ -21,16 +37,139 @@ const BrokerageOutputs = (props: { chargesBreakDown: Charges; }) => {
         profit, setProfit
     ] = useState<string | number>(1010);
 
+    const decimal = (item: any, field: string) => {
+        if (item.toString().includes(".")) {
+            switch (field) {
+                case "turnover":
+                    setTurnover(Number(item).toFixed(2));
+                    break;
+
+                case "brokerage":
+                    setBrokerageValue(Number(item).toFixed(2));
+                    break;
+                
+                case "bep":
+                    setBEP(Number(item).toFixed(2));
+                    break;
+
+                case "profit":
+                    setProfit(Number(item).toFixed(2));
+                    break;
+                default:
+                    break;
+            }
+        }      
+    };
+
     useEffect(() => {
         if (chargesBreakDown && chargesBreakDown.brokerage) {
             setBrokerageValue(chargesBreakDown.brokerage);
             setBEP(chargesBreakDown.pointBreakeven);
             setProfit(chargesBreakDown.netProfit);
             setTurnover(chargesBreakDown.turnOver);
+            console.log(chargesBreakDown);
+        } else {
+            setTurnover("0");
+            setBrokerageValue("0");
+            setBEP("0");  
+            setProfit("0");  
         }
-        
+        document.querySelectorAll(".field-inputs").forEach((item) => {
+            console.log(item);
+            item.removeAttribute("class");
+            item.classList.add("digit-default");
+            item.classList.add("field-inputs");
+        });
     }, [
         chargesBreakDown
+    ]);
+
+    useEffect(() => {
+        decimal(turnover, "turnoverVal");
+        const arr = [
+
+        ];
+        for (let val: any = 0; val <= turnover.toString().length; val++) {
+            arr.push(turnover.toString()[ val ]);
+        }
+        decimal(Number(arr), "turnover");
+        setTurnoverArr(arr);
+    }, [
+        turnover
+    ]);
+
+    useEffect(() => {
+        decimal(brokerageValue, "brokerage");
+        const arr = [
+
+        ];
+        for (let val: any = 0; val <= brokerageValue.toString().length; val++) {
+            arr.push(brokerageValue.toString()[ val ]);
+        }
+        decimal(Number(arr), "brokerage");
+        setBrokerageArr(arr);
+    }, [
+        brokerageValue
+    ]);
+
+    useEffect(() => {
+        const arr = [
+
+        ];
+        for (let val: any = 0; val <= bep.toString().length; val++) {
+            arr.push(bep.toString()[ val ]);
+        }
+        decimal(Number(arr), "bep");
+        setBepArr(arr);
+    }, [
+        bep
+    ]);
+
+    useEffect(() => {
+        const arr = [
+
+        ];
+        for (let val: any = 0; val <= profit.toString().length; val++) {
+            arr.push(profit.toString()[ val ]);
+        }
+        decimal(Number(arr), "profit");
+        setProfitArr(arr);
+    }, [
+        profit
+    ]);
+
+    useEffect(() => {
+        document.querySelectorAll(".field-inputs").forEach((item) => {
+            item.removeAttribute("class");
+            item.classList.add("field-inputs");
+            if (item.innerHTML === "0") {
+                item.classList.add("digitnone");
+            } else if (item.innerHTML === "1") {
+                item.classList.add("digit-1");
+            } else if (item.innerHTML === "2") {
+                item.classList.add("digit-2");
+            } else if (item.innerHTML === "3") {
+                item.classList.add("digit-3");
+            } else if (item.innerHTML === "4") {
+                item.classList.add("digit-4");
+            } else if (item.innerHTML === "5") {
+                item.classList.add("digit-5");
+            } else if (item.innerHTML === "6") {
+                item.classList.add("digit-6");
+            } else if (item.innerHTML === "7") {
+                item.classList.add("digit-7");
+            } else if (item.innerHTML === "8") {
+                item.classList.add("digit-8");
+            } else if (item.innerHTML === "9") {
+                item.classList.add("digit-9");
+            } else if (item.innerHTML === "-") {
+                item.classList.add("digit-hyphen");
+            } else if (item.innerHTML === ".") {
+                item.classList.add("digit-point");
+            }
+        });
+    }, [
+        turnoverArr, brokerageArr, bepArr, profitArr
     ]);
 
     return (
@@ -38,14 +177,27 @@ const BrokerageOutputs = (props: { chargesBreakDown: Charges; }) => {
             <div className="top-output-container">
                 <div className="brokerage-input-section input-container">
                     <div className="brokerage-label">Brokerage</div>
-                    <div className="brokerage-input output-bg">
-                        {brokerageValue}
+                    <div className="brokerage-input output-bg" id="value-0">
+                        <span className="icon-rupee"></span>
+                        {brokerageArr && brokerageArr.map((item: any, key: number) => {
+                            return (
+                                <span className="field-inputs" id={item} key={key}>
+                                    {item}
+                                </span>
+                            );
+                        })}              
                     </div>
                 </div>
                 <div className="turnover-input-section input-container">
                     <div className="turnover-label">Turnover</div>
-                    <div className="turnover-input output-bg">
-                        {turnover}
+                    <div className="output-bg" id="turnover-digit-0">
+                        {turnoverArr && turnoverArr.map((item: any, key: number) => {
+                            return (
+                                <span className="field-inputs" id={item} key={key}>
+                                    {item}
+                                </span>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
@@ -54,17 +206,30 @@ const BrokerageOutputs = (props: { chargesBreakDown: Charges; }) => {
                 <div className="bep-input-section input-container">
                     <div className="bep-label">BEP</div>
                     <div className="bep-input output-bg">
-                        {bep}
+                        {bepArr && bepArr.map((item: any, key: number) => {
+                            return (
+                                <span className="field-inputs" id={item} key={key}>
+                                    {item}
+                                </span>
+                            );
+                        })}
                     </div>
                 </div>
                 <div className="profit-input-section input-container">
                     <div className="profit-label">Profit</div>
                     <div className="profit-input output-bg">
-                        {profit}
+                        <span className="icon-rupee"></span>
+                        {profitArr && profitArr.map((item: any, key: number) => {
+                            return (
+                                <span className="field-inputs" id={item} key={key}>
+                                    {item}
+                                </span>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
-        
+
         </div>
     );
 };
--- src/components/brokerage/KnowBrokerageSavingsComponent.tsx
+++ src/components/brokerage/KnowBrokerageSavingsComponent.tsx
@@ -1,7 +1,9 @@
+import { Charges, InputTypes } from "common/Types";
 import { COMMODITY_CATEGORY, CURRENCY_CATEGORY, EQUITES_CATEGORY } from "common/Constants";
 import React, { useEffect, useState } from "react";
+import InputText from "components/common/InputTextComponent";
 
-const KnowBrokerageSavings = (props: { inputKeys: any; chargesList: any; }) => {
+const KnowBrokerageSavings = (props: { inputKeys: InputTypes; chargesList: Charges; }) => {
 
     const { inputKeys, chargesList } = props;
     const [
@@ -12,8 +14,11 @@ const KnowBrokerageSavings = (props: { inputKeys: any; chargesList: any; }) => {
         savingsOutput, setSavingsOutput
     ] = useState<number>();
 
-    const calculateYourSavings = (brokeragePersent: number) => {
+    const [
+        savingsArr, setSavingsArr
+    ] = useState<any>();
 
+    const calculateYourSavings = (brokeragePersent: number) => {
         console.log("inputKeys", inputKeys);
 
         const { sellPrice, quantity, buyPrice, type } = inputKeys;
@@ -32,7 +37,7 @@ const KnowBrokerageSavings = (props: { inputKeys: any; chargesList: any; }) => {
 
         } else if (type === CURRENCY_CATEGORY[ 1 ].name || (type === COMMODITY_CATEGORY[ 0 ].name)) {
 
-            totalSavings = (( sellPrice * quantity ) + ( buyPrice * quantity )) * brokeragePersent;
+            totalSavings = (( sellPrice * quantity ) + ( buyPrice * quantity ) ) * brokeragePersent;
             // totalSavings = ( sellPrice * quantity ) + ( buyPrice * quantity ) * brokeragePersent;
 
         } else {
@@ -44,17 +49,70 @@ const KnowBrokerageSavings = (props: { inputKeys: any; chargesList: any; }) => {
 
     useEffect(() => {
         // setCurrentBrokerage(40);
-        calculateYourSavings(currentBrokerage);            
+        calculateYourSavings(currentBrokerage);           
     }, [
         props.inputKeys.type
     ]);
+
     
-    const onChangebrokerageValue = (evt: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentBrokerage(Number(evt.target.value));
-        setSavingsOutput(98);
-        calculateYourSavings(Number(evt.target.value));
+    const onChangebrokerageValue = (brokValue: number) => {
+        setCurrentBrokerage(brokValue);
+        // setSavingsOutput(98);
+        calculateYourSavings(brokValue);
     };
     
+    useEffect(() => {
+        document.querySelectorAll(".counter-inputs").forEach((item) => {
+            item.removeAttribute("class");
+            item.classList.add("counter-inputs");
+            item.classList.add("digit-default");
+        }); 
+        const arr = [
+
+        ];
+        if (savingsOutput) {
+            for (let val: any = 0; val <= savingsOutput.toString().length; val++) {
+                arr.push(savingsOutput.toString()[ val ]);
+            }
+        }
+        setSavingsArr(arr);
+    }, [
+        savingsOutput
+    ]);
+
+    useEffect(() => {
+        document.querySelectorAll(".counter-inputs").forEach((item) => {
+            item.removeAttribute("class");
+            item.classList.add("counter-inputs");
+            if (item.innerHTML === "0") {
+                item.classList.add("digitnone");
+            } else if (item.innerHTML === "1") {
+                item.classList.add("digit-1");
+            } else if (item.innerHTML === "2") {
+                item.classList.add("digit-2");
+            } else if (item.innerHTML === "3") {
+                item.classList.add("digit-3");
+            } else if (item.innerHTML === "4") {
+                item.classList.add("digit-4");
+            } else if (item.innerHTML === "5") {
+                item.classList.add("digit-5");
+            } else if (item.innerHTML === "6") {
+                item.classList.add("digit-6");
+            } else if (item.innerHTML === "7") {
+                item.classList.add("digit-7");
+            } else if (item.innerHTML === "8") {
+                item.classList.add("digit-8");
+            } else if (item.innerHTML === "9") {
+                item.classList.add("digit-9");
+            } else if (item.innerHTML === "-") {
+                item.classList.add("digit-hyphen");
+            } else if (item.innerHTML === ".") {
+                item.classList.add("digit-point");
+            }
+        });
+    }, [
+        savingsArr
+    ]);
 
     return (
         <div className="know-brokerage-savings-container">
@@ -62,13 +120,15 @@ const KnowBrokerageSavings = (props: { inputKeys: any; chargesList: any; }) => {
                 WHAT IS YOUR PRESENT BROKERAGE?
             </div>
             <div className="brokerage-present-input">
-                <input 
-                    type="text"
-                    value={currentBrokerage}
-                    onChange={(evt) => {
-                        return onChangebrokerageValue(evt); 
+               
+                <InputText 
+                    parentCallBack={(val) => {
+                        return onChangebrokerageValue(Number(val)); 
                     }}
+                    ipValue={currentBrokerage}
+                    numbersOnly = {true}
                 />
+                
                 <span className="or-text">OR</span>
             </div>
             <div className="your-savings">
@@ -76,7 +136,14 @@ const KnowBrokerageSavings = (props: { inputKeys: any; chargesList: any; }) => {
                     Your Savings
                 </div>
                 <div className="your-savings-output">
-                    {savingsOutput}
+                    <span className="icon-rupee"></span>
+                    {savingsArr && savingsArr.map((item: any, key: number) => {
+                        return (
+                            <span className="counter-inputs" id={item} key={key}>
+                                {item}
+                            </span>
+                        );
+                    })}
                 </div>
             </div>
         </div>
